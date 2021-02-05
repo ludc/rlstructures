de818f60006b8ad9c70bb5b0f036309ca6d766ce
Ludovic Denoyer <denoyer@fb.com>
2021-02-05 16:19:52 +0100

    coucou

---
+++ e_rlalgos/dqn/pong_cfgs.py
@@ -0,0 +1,42 @@
+(
+    {"environment/env_name": "PongNoFrameskip-v4",
+            "n_envs": 1,
+            "max_episode_steps": 10000,
+            "discount_factor": 0.99,
+            "epsilon_greedy_max": 0.9,
+            "epsilon_greedy_min": 0.01,
+            "epsilon_min_epoch": [1000000,200000],
+            "replay_buffer_size": [100000],
+            "n_batches": 32,
+            "use_duelling": [False,True],
+            "use_double": [False,True],
+            "lr": [0.0001,0.00003],
+            "update_target_epoch":[200,1000],
+            "n_evaluation_processes": 4,
+            "verbose": True,
+            "n_evaluation_envs": 4,
+            "time_limit": 28800,
+            "env_seed": 48,
+            "clip_grad": [0.0,10.0],
+            "learner_device": "gpu",
+            "logdir":"./results"
+    }
+,
+    [
+        {
+            "initial_buffer_epochs": 10000,
+            "qvalue_epochs": 10,
+            "batch_timesteps": 10,
+            "n_processes": 1,
+        }
+        ,
+        {
+            "initial_buffer_epochs": 10000,
+            "qvalue_epochs": 10,
+            "batch_timesteps": 10,
+            "n_processes": 1,
+        }
+        ,
+
+    ]
+)
