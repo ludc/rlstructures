c6f5bf5ec890568829b8d4e7b7d8a7555213ff20
Ludovic Denoyer <denoyer@fb.com>
2021-02-11 12:43:26 +0100

    updte q

---
+++ rlalgos/dqn/run_atari_gpu_on_slurm.py
@@ -0,0 +1,133 @@
+from rlstructures import logging
+from rlstructures.env_wrappers import GymEnv, GymEnvInf
+from rlstructures.tools import weight_init
+from rlstructures.batchers import Batcher,EpisodeBatcher
+import torch.nn as nn
+import copy
+import torch
+import time
+import numpy as np
+import torch.nn.functional as F
+from rlalgos.dqn.agent import QAgent, QMLP, DQMLP, DuelingCnnDQN, CnnDQN
+import gym
+from gym.wrappers import TimeLimit
+from rlalgos.dqn.duelling_dqn import DQN
+from rlalgos.atari_wrappers import make_atari, wrap_deepmind, wrap_pytorch
+import math
+import itertools
+
+def create_env(n_envs, mode="train",max_episode_steps=None, seed=None,**args):
+
+
+    if mode=="train":
+        envs=[]
+        for k in range(n_envs):
+            e = make_atari(args["environment/env_name"])
+            e = wrap_deepmind(e)
+            e = wrap_pytorch(e)
+            envs.append(e)
+        return GymEnvInf(envs, seed)
+    else:
+        envs=[]
+        for k in range(n_envs):
+            e = make_atari(args["environment/env_name"])
+            e = wrap_deepmind(e)
+            e = wrap_pytorch(e)
+            envs.append(e)
+        return GymEnv(envs, seed)
+
+def create_agent(n_actions=None, model=None, ):
+    return QAgent(model=model, n_actions=n_actions)
+
+class Experiment(DQN):
+    def __init__(self, config, create_env, create_agent):
+        super().__init__(config, create_env, create_agent)
+
+    def _create_model(self):
+        if (self.config["use_duelling"]):
+            module = DuelingCnnDQN(self.obs_shape,self.n_actions)
+        else:
+            module = CnnDQN(self.obs_shape,self.n_actions)
+        #module.apply(weight_init)
+        return module
+
+def product_dict(**kwargs):
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    for instance in itertools.product(*vals):
+        yield dict(zip(keys, instance))
+
+def generate_grid_search(config):
+    c = {}
+    for k in config:
+        if not isinstance(config[k], list):
+            c[k] = [config[k]]
+        else:
+            c[k] = config[k]
+    return product_dict(**c)
+
+def generate(common,specifics):
+    gs=list(generate_grid_search(common))
+    print("Common ",len(gs)," Specific ",len(specifics))
+    r=[]
+    for s in specifics:
+        for c in gs:
+            cc=copy.deepcopy(c)
+            for k in s:
+                cc[k]=s[k]
+            r.append(cc)
+    print("== Total ",len(r))
+    return r
+
+if __name__=="__main__":
+    #We use spawn mode such that most of the environment will run in multiple processes
+    import torch.multiprocessing as mp
+    mp.set_start_method("spawn")
+
+    config={"environment/env_name": "PongNoFrameskip-v4",
+            "n_envs": 1,
+            "max_episode_steps": 10000,
+            "discount_factor": 0.99,
+            "epsilon_greedy_max": 0.5,
+            "epsilon_greedy_min": 0.1,
+            "epsilon_min_epoch": 1000000,
+            "replay_buffer_size": 100000,
+            "n_batches": 32,
+            "update_target_epoch":1000,
+            "tau": 0.005,
+            "initial_buffer_epochs": 1,
+            "qvalue_epochs": 10,
+            "batch_timesteps": 10,
+            "use_duelling": False,
+            "use_double":False,
+            "lr": 0.0001,
+            "n_processes": 1,
+            "n_evaluation_processes": 4,
+            "verbose": True,
+            "n_evaluation_envs": 4,
+            "time_limit": 28800,
+            "env_seed": 48,
+            "clip_grad": 0.0,
+            "learner_device": "cuda",
+            "logdir":"./results",
+            "update_target_epoch":1000
+
+    }
+    import sys
+    import os
+    print(len(sys.argv))
+    if len(sys.argv)==2:
+        print("opening ",sys.argv[1])
+        _file = open(sys.argv[1], "r")
+        c=_file.read()
+        _file.close()
+        common,specifics=eval(c)
+        r=generate(common,specifics)
+        if os.environ.get("SLURM_ARRAY_TASK_ID")==None:
+            exit()
+        config=r[int(os.environ.get("SLURM_ARRAY_TASK_ID"))]
+        logdir=config["logdir"]
+        config["logdir"]=logdir+"/"+str(int(os.environ.get("SLURM_ARRAY_TASK_ID")))
+
+    exp=Experiment(config,create_env,create_agent)
+    exp.run()
---
+++ rlalgos/dqn/run_cartpole.py
@@ -0,0 +1,96 @@
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+
+from rlstructures import logging
+from rlstructures.env_wrappers import GymEnv, GymEnvInf
+from rlstructures.tools import weight_init
+from rlstructures.batchers import Batcher,EpisodeBatcher
+import torch.nn as nn
+import copy
+import torch
+import time
+import numpy as np
+import torch.nn.functional as F
+from rlalgos.dqn.agent import QAgent, QMLP, DQMLP
+import gym
+from gym.wrappers import TimeLimit
+from rlalgos.dqn.duelling_dqn import DQN
+
+def create_gym_env(args):
+    return gym.make(args["environment/env_name"])
+
+def create_env(n_envs, mode="train",max_episode_steps=None, seed=None,**args):
+    envs=[]
+    for k in range(n_envs):
+        e = create_gym_env(args)
+        e = TimeLimit(e, max_episode_steps=max_episode_steps)
+        envs.append(e)
+
+    if mode=="train":
+        return GymEnvInf(envs, seed)
+    else:
+        return GymEnv(envs, seed)
+
+def create_agent(n_actions=None, model=None, ):
+    return QAgent(model=model, n_actions=n_actions)
+
+class Experiment(DQN):
+    def __init__(self, config, create_env, create_agent):
+        super().__init__(config, create_env, create_agent)
+
+    def _create_model(self):
+        module=None
+        if (not self.config["use_duelling"]):
+            module = QMLP(self.obs_shape[0], self.n_actions, 64)
+        else:
+            module = DQMLP(self.obs_shape[0], self.n_actions, 64)
+
+        module.apply(weight_init)
+        return module
+
+def flatten(d, parent_key='', sep='/'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, DictConfig):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+if __name__=="__main__":
+    #We use spawn mode such that most of the environment will run in multiple processes
+    import torch.multiprocessing as mp
+    mp.set_start_method("spawn")
+
+    config={"environment/env_name": "CartPole-v0",
+            "n_envs": 4,
+            "max_episode_steps": 100,
+            "discount_factor": 0.9,
+            "epsilon_greedy_max": 0.3,
+            "epsilon_greedy_min": 0.01,
+            "replay_buffer_size": 1000,
+            "n_batches": 64,
+            "tau": 0.005,
+            "initial_buffer_epochs": 10,
+            "qvalue_epochs": 10,
+            "batch_timesteps": 1,
+            "use_duelling": True,
+            "lr": 0.001,
+            "n_processes": 4,
+            "n_evaluation_processes": 4,
+            "verbose": True,
+            "n_evaluation_envs": 16,
+            "time_limit": 1800,
+            "env_seed": 42,
+            "clip_grad": 40.0,
+            "learner_device": "cpu",
+            "logdir":"./results"
+    }
+    exp=Experiment(config,create_env,create_agent)
+    exp.run()
