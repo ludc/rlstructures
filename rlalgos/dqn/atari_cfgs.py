08405792c9fe49c79dc9095d3249e83f6c33fd34
Ludovic Denoyer <denoyer@fb.com>
2021-02-12 15:33:37 +0100

    update simple DQN

--- rlalgos/dqn/atari_cfgs.py
+++ rlalgos/dqn/atari_cfgs.py
@@ -1,59 +1,81 @@
 (
     {"environment/env_name": ["PongNoFrameskip-v4","AsteroidsNoFrameskip-v4","BoxingNoFrameskip-v4","SeaquestNoFrameskip-v4","TennisNoFrameskip-v4","JamesbondNoFrameskip-v4"],
-            "n_envs": 1,
+            #n environment per process
+            "n_envs": [1,4],
+            #Maximum number of timesteps (for evaluation episodes)
             "max_episode_steps": 15000,
+            #Discount factor
             "discount_factor": 0.99,
-            "epsilon_greedy_max": 0.99,
-            "epsilon_greedy_min": [0.1,0.01],
-            "epsilon_min_epoch": [200000,2000000],
+            #Epsilon value at the beginning of the learning process
+            "epsilon_greedy_max": 1.0,
+            #Epsilon value at the end of the decay
+            "epsilon_greedy_min": [0.01],
+            #Learning epoch when the min epsilon value is reached
+            "epsilon_min_epoch": [100000,400000],
+            # Size of the replay buffer (number of transitions)
             "replay_buffer_size": [100000],
-            "n_batches": 32,
+            # Size of the learning batches
+            "n_batches": [32,128],
+            #Duelling architecture ?
             "use_duelling": [False],
+            #Double DQN ?
             "use_double": [True],
-            "lr": [1e-4,3e-5],
+            #Learning rate
+            "lr": [1e-4,1e-5],
+            #N env per evaluation process:   n_evaluation_processes*n_evaluation_envs is the number of trajectories used for each evaluation
+            "n_evaluation_envs": 1,
+            #N paralellel evaluation processes: increase if you wnat more frequent evaluation
             "n_evaluation_processes": 4,
-            "verbose": True,
-            "optim":["RMSprop","Adam"],
-            "n_evaluation_envs": 4,
-            "time_limit": 36000,
-            "env_seed": 48,
-            "clip_grad": [2.0],
-            "learner_device": "cuda",
 
+            "verbose": False,
+            #Optimizer name
+            "optim":["Adam"],
+            #Stop the process after n seconds
+            "time_limit": 43200,
+            #Env seed
+            "env_seed": 48,
+            #Clip gradient
+            "clip_grad": [0.0],
+            #cpu or cuda
+            "learner_device": "cuda",
+            # if True, the system will do as many model updates as possible
+            "as_fast_as_possible":[True],
+
+            #Update target model is a hard way (by copying) or with smooth upddates. If True=>update_target_epoch if False=>update_target_tau
             "update_target_hard":[True],
-            "update_target_epoch":1000,
+            #Copy to target model every n epochs
+            "update_target_epoch":100,
+            #Smooth update coefficient of the target model (i.e 0.005)
             "update_target_tau": 0.005,
 
-            "logdir":"/checkpoint/denoyer/atari",
-            "save_every":1000,
+            "logdir":"/checkpoint/denoyer/dqn_atari",
+            #Save logged values every n timesteps (to avoid too large log files)
+            "save_every":100,
     }
 ,
     [
         {
-            "initial_buffer_epochs": 600,
+            "initial_buffer_epochs": 2500,
             "qvalue_epochs": 1,
-            "batch_timesteps": 4,
-            "n_processes": 4,
-            "buffer/alpha":0.,
-            "buffer/beta":0.,
-            "as_fast_as_possible":False,
-        }
-        ,
-        {
-            "initial_buffer_epochs": 600,
-            "qvalue_epochs": 4,
             "batch_timesteps": 4,
             "n_processes": 4,
             "buffer/alpha":0.0,
             "buffer/beta":0.0,
-            "as_fast_as_possible":False,
         }
         ,
         {
             "initial_buffer_epochs": 600,
             "qvalue_epochs": 1,
+            "batch_timesteps": 10,
+            "n_processes": 4,
+            "buffer/alpha":0.0,
+            "buffer/beta":0.0,
+        }
+        ,
+        {
+            "initial_buffer_epochs": 300,
+            "qvalue_epochs": 1,
             "batch_timesteps": 20,
-            "as_fast_as_possible":True,
             "n_processes": 4,
             "buffer/alpha":0.0,
             "buffer/beta":0.0,
