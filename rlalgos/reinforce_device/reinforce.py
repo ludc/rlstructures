5679ba7a07e681596f94ed42667150dd888397a7
Ludovic Denoyer <denoyer@fb.com>
2021-02-23 11:29:03 +0100

    test GPU

--- rlalgos/reinforce_device/reinforce.py
+++ rlalgos/reinforce_device/reinforce.py
@@ -127,7 +127,7 @@ class Reinforce:
 
         # Update the batcher with the last version of the learning model
 
-        sd=self._state_dict(self.learning_model,torch.device("cpu")) #self._state_dict(self.learning_model,self.config["batcher_device"])
+        sd=self.learning_model.state_dict() #self._state_dict(self.learning_model,self.config["batcher_device"])
         self.train_batcher.update(sd)
 
         n_interactions = 0
@@ -170,8 +170,7 @@ class Reinforce:
             optimizer.step()
 
             # 6) Update the train batcher with the updated model
-#            sd=self._state_dict(self.learning_model,self.config["batcher_device"])
-            sd=self._state_dict(self.learning_model,torch.device("cpu")) #self._state_dict(self.learning_model,self.config["batcher_device"])
+            sd=self.learning_model.state_dict() #self._state_dict(self.learning_model,torch.device("cpu")) #self._state_dict(self.learning_model,self.config["batcher_device"])
             self.train_batcher.update(sd)
 
             # 7) Print some messages
--- rlalgos/reinforce_device/run_reinforce.py
+++ rlalgos/reinforce_device/run_reinforce.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
         "reinforce_coef": 1.0,
         "evaluation_mode": "stochastic",
         "logdir": "./results",
-        "learner_device":torch.device("cpu"),
+        "learner_device":torch.device("cuda:0"),
         "batcher_device":torch.device("cuda:1"),
         "evaluation_device":torch.device("cpu")
     }
